ce2a4d27794fc6185b0a7df70f2be4c9be55484a
Douglas Martinez <dougie.martinez.m@gmail.com>
2014-11-06 12:08:53 -0800

    submit button addded

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+    require_once(__DIR__. "/view/header.php");
+
+    require_once(__DIR__. "/view/navigation.php");
+
+    require_once(__DIR__. "/controller/create-db.php");
+
+    require_once(__DIR__. "/view/footer.php")
+?>
+
+<!--insert header code in PHP file -->
+<!--links our create-db file to our index --!>
+<!--insert footer code in PHP file --!>
+<!--links navigation on the blog--!>

87bd3562d1dafaccf5cac8f396c3779cb11938bf
Douglas Martinez <dougie.martinez.m@gmail.com>
2014-11-12 12:02:11 -0800

    video 23; created a require_once from the create-post file and made a connection to the database file from the view folder.

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,7 +1,13 @@
 <?php
+    require_once(__DIR__ . "/../model/database.php");
+    //requires database file from the model folder
+        
+    $connection = new mysqli($host, $username, $password, $database);
+        
     $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
     $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
     
     echo  "<p>Title: $title</p>";
     echo  "<p>Post: $post</p>";
 
+    $connection->close();

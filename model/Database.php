2f18df974a33207418d643983c96e0f806db0bbd
Douglas Martinez <dougie.martinez.m@gmail.com>
2014-11-18 12:14:36 -0800

    added the isset function finished video 29,30 . Made new mysqli for password, username, host, and database

--- model/Database.php
+++ model/Database.php
@@ -1,33 +1,46 @@
 <?php
+
 //setting the class to private makes this viewable only in thid file
 class Database {
-     private $connection;
-     private $host;
-     private $username;
-     private $password;
-     private $database;
-     //passing the constructed infromation
-     public function __construct($host, $username, $password, $database ) {
-         $this->host = $host;
-         $this->username = $username;
-         $this->password = $username;
-         $this->database = $database;
-         //created functions
-         //the function__construct allows you to pass any parameters on object construction
-     }
-     
-     public function openConnecction () {
-         
-     }
-     
-     public function closeConnection () {
-         
-     }
-     
-     public function query($string) {
-         
-     }
+
+    private $connection;
+    private $host;
+    private $username;
+    private $password;
+    private $database;
+
+//passing the constructed infromation
+    public function __construct($host, $username, $password, $database) {
+        $this->host = $host;
+        $this->username  = $username;
+        $this->password  = $username;
+        $this->database  = $database;
+//created functions
+//the function__construct allows you to pass any parameters on object construction
+    }
+
+    public function openConnecction() {
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+        
+        if($this->connection->connect_error) {
+             die("<p>Error: " . $this->connection->connect_error . "</p>");
+        }
+          
+    }
+//the isset lets you check if there is information present in the variable
+    public function closeConnection() {
+        if(issset($this->connection)) {
+            $this->connection->close();
+        }
+        
+    }
+
+    public function query($string) {
+        
+    }
+
 }
+
 //we made private classes so that we can make it viewable in this file and they cant be modified
 //makes it easier to read 
 //easier to maintain
